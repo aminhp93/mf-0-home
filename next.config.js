583a1da30699fa88ff819e996d7d655a618cd0ea
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2023-11-17 08:40:09 +0100

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
+let host = "http://localhost:3001";
+
+if (process.env.NODE_ENV === "production") {
+  host = "https://home.vercel.app";
 }
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config, options) {
+    // if (!options.isServer) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "home",
+        filename: "static/chunks/remoteEntry.js",
+        remotes: {
+          common: `common@${host}/_next/static/chunks/remoteEntry.js`,
+          // authentication:
+          //   "authentication@http://localhost:3002/_next/static/chunks/remoteEntry.js",
+        },
+        exposes: {},
+        shared: {},
+      })
+    );
+    // }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;

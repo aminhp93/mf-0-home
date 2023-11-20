b41eb2b3a5e2d1c46ff4f8549338c686199f8fa1
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2023-11-20 07:18:34 +0100

    Update

--- components/RemoteItems.tsx
+++ components/RemoteItems.tsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
-// load content of all files in components folder
-let xxx: any;
-// @ts-ignore
-import("items/components").then((res) => {
-  console.log(res);
-  xxx = res;
-});
+// // load content of all files in components folder
+// let xxx: any;
+// // @ts-ignore
+// import("items/components").then((res) => {
+//   console.log(res);
+//   xxx = res;
+// });
 
 const Page = () => {
   const [isClient, setIsClient] = useState(false);
+  const [xxx, setXxx] = useState<any>(null);
   useEffect(() => {
     setIsClient(true);
   }, []);
-  console.log({ isClient, xxx });
-  if (!isClient) return <div></div>;
-  if (!xxx) return <div></div>;
+
+  useEffect(() => {
+    // load content of all files in components folder
+    let xxx: any;
+    // @ts-ignore
+    import("items/components").then((res) => {
+      console.log(res);
+      xxx = res;
+      setXxx(xxx);
+    });
+  }, []);
+
+  console.log({ isClient });
+  if (!isClient || !xxx) return <div></div>;
 
   return (
     <div>

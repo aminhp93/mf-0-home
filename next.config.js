9fdce9c6be2dd815148bdfaef0ffdae99e26cf36
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2023-11-29 09:51:04 +0100

    Update

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ let hostCommon = "http://localhost:3001";
 let hostItems = "http://localhost:3002";
 let hostProperty = "http://localhost:3003";
 
-if (process.env.NODE_ENV !== "production") {
-  // hostCommon = "https://common-omega.vercel.app";
+if (process.env.NODE_ENV === "production") {
+  hostCommon = "https://common-omega.vercel.app";
   hostItems = "https://items-lilac.vercel.app";
-  // hostProperty = "https://property-tau.vercel.app";
+  hostProperty = "https://property-tau.vercel.app";
 }
 
 const nextConfig = {

0ab8a1eaf7143d6fc296358b469b061e97761c17
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2023-12-05 04:29:32 +0100

    Update

--- next.config.js
+++ next.config.js
@@ -4,16 +4,6 @@ const {
   createSharedDependencies,
 } = require("./@core/configs/next-config-util.js");
 
-let hostCommon = process.env.HOST_COMMON_LOCAL;
-let hostItems = process.env.HOST_ITEMS_LOCAL;
-let hostProperty = process.env.HOST_PROPERTY_LOCAL;
-
-if (process.env.NODE_ENV === "production") {
-  hostCommon = process.env.HOST_COMMON_PRODUCTION;
-  hostItems = process.env.HOST_ITEMS_PRODUCTION;
-  hostProperty = process.env.HOST_PROPERTY_PRODUCTION;
-}
-
 const nextConfig = {
   reactStrictMode: true,
   webpack(config, options) {
